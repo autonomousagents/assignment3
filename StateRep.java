759e1d4b63edfdbf3ef27d632d0e2dc2fa704aa5
Agnes van Belle <agnesvanbelle@gmail.com>
2012-10-10 15:54:23 +0200

    bla

--- StateRep.java
+++ StateRep.java
@@ -5,32 +5,33 @@ public class StateRep {
 	
 	private boolean isPrey;
 	private int nrOtherPredators;
+	private RelativeStateRep relativeStateRep; // own position w.r.t. prey, or (if himself is prey) w.r.t. first predator
+	private double initialValue;
 	
 	//private double[][][] oneOtherPredator; //[Environment.HEIGHT][Environment.WIDTH][Direction.nrMoves];
 	private ArrayList<double[][][]> otherPredators;
 	
-	 public StateRep (boolean isPrey, int nrPredators ){
+	 public StateRep (double init, boolean isPrey, int nrPredators ){
        // stateRep = new double[stateRepHeight][stateRepWidth][nrActions];
       //  for(double[][] m: stateRep)
         //	for(double[] row: m)Arrays.fill(row, init);
       //  fillUnused();
 		 
-		 this.isPrey = isPrey;
+		 this.isPrey = isPrey;	 
+		
+		 this.nrOtherPredators = nrPredators-1;
+		 this.initialValue = init;
 		 
-		 if (isPrey)
-			 this.nrOtherPredators = nrPredators;
-		 else
-			 this.nrOtherPredators = nrPredators-1;
-		 
+		 relativeStateRep = new RelativeStateRep(initialValue);
 		
     }
 	 
 	 private void fillOtherPredators() {
 		 otherPredators = new ArrayList<double[][][]>();
 		 
-		 for (int i =0; i < nrPredators; i++) {
+		 for (int i =0; i < nrOtherPredators; i++) {
 			 double[][][] otherPredator = new double[Environment.HEIGHT][Environment.WIDTH][Direction.nrMoves];
-			 otherPredators.add(otherPredator)		;	 
+			 otherPredators.add(otherPredator)	;	 
 		 }
 	 }
 	 
